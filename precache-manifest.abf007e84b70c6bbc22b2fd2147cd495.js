3b9aab36607cee08c4885da411b6cbf6f5cccff7
chan <thechanmoon@gmail.com>
2020-07-17 17:09:01 -0400

    Updates

---
+++ precache-manifest.abf007e84b70c6bbc22b2fd2147cd495.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9596d4b6f72d4ca7d588",
+    "url": "/video-db-react/static/js/main.9596d4b6.chunk.js"
+  },
+  {
+    "revision": "c508dcb54a3f678e39dc",
+    "url": "/video-db-react/static/js/1.c508dcb5.chunk.js"
+  },
+  {
+    "revision": "fa31a17f9badb7838644",
+    "url": "/video-db-react/static/js/runtime~main.fa31a17f.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/video-db-react/static/media/noPosterSmall.87a0eb6a.png"
+  },
+  {
+    "revision": "ff69dae55cf40156315cae103fc740b3",
+    "url": "/video-db-react/index.html"
+  }
+];
